3a66dac635d058f54641679e87bcc351b3e7ee4e
paolo.spadoni <Paolo>
2021-03-27 10:29:26 +0100

    section-8-error-handling

--- src/main/java/com/spring/graphql/resolver/bank/ClientResolver.java
+++ src/main/java/com/spring/graphql/resolver/bank/ClientResolver.java
@@ -3,6 +3,8 @@ package com.spring.graphql.resolver.bank;
 import com.spring.graphql.domain.bank.BankAccount;
 import com.spring.graphql.domain.bank.Client;
 import com.spring.graphql.exceptions.GraphqlException;
+import graphql.execution.DataFetcherResult;
+import graphql.kickstart.execution.error.GenericGraphQLError;
 import graphql.kickstart.tools.GraphQLResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,20 @@ import java.util.UUID;
 @Component
 public class ClientResolver implements GraphQLResolver<BankAccount> {
 
-    public Client client(BankAccount bankAccount){
+    /*public Client client(BankAccount bankAccount){
         log.info("requesting client data for bank account id = " + bankAccount.getId());
-        throw new RuntimeException("Spring exception");
+        // new RuntimeException("Spring exception");
         //throw new GraphqlException("Client unavailable");
-        //return Client.builder().id(UUID.randomUUID()).firstName("Paul").lastName("Spash").build();
+        return Client.builder().id(UUID.randomUUID()).firstName("Paul").lastName("Spash").build();
+    }*/
+
+    public DataFetcherResult<Client> client(BankAccount bankAccount) {
+        log.info("requesting client data for bank account id = " + bankAccount.getId());
+
+        return DataFetcherResult.<Client>newResult()
+                .data(Client.builder().id(UUID.randomUUID()).firstName("Paul").lastName("Spash").build())
+        .error(new GenericGraphQLError("could not get sub client id"))
+        .build();
     }
 }
+
